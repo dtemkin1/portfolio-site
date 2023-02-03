ef121495c369b1b4c7095d3f64d33fae0333bcf3
Dgoat724236 <dtemkin@mit.edu>
2023-02-03 18:46:42 -0500

    Configure for custom domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://dtemkin1.github.io',
+    site: 'https://diegotemkin.com',
 });
