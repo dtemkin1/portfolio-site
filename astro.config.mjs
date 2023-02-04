c7f4db259fc6cae4406896e626a045c01f807b6d
Dgoat724236 <dtemkin@mit.edu>
2023-02-03 19:43:13 -0500

    Add preload, change profile aspect ratio

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,12 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
+import prefetch from "@astrojs/prefetch";
+
+// https://astro.build/config
 export default defineConfig({
-    site: 'https://diegotemkin.com',
+  site: 'https://diegotemkin.com',
+  integrations: [prefetch({
+    throttle: 3
+  })]
 });
