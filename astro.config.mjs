9d7a20eb1f6055262dceeac965672d2f7375e20f
Dgoat724236 <dtemkin@mit.edu>
2023-02-14 23:47:11 -0500

    Add compress, change major (again :sob:)

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,9 +4,12 @@ import { defineConfig } from 'astro/config';
 import prefetch from "@astrojs/prefetch";
 
 // https://astro.build/config
+import compress from "astro-compress";
+
+// https://astro.build/config
 export default defineConfig({
   site: 'https://diegotemkin.com',
   integrations: [prefetch({
     throttle: 3
-  })]
+  }), compress()]
 });
