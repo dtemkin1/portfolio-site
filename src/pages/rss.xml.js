7a0bb2c7f5461974ff57d54199911fadf104e547
Dgoat724236 <dtemkin@mit.edu>
2023-07-04 20:40:02 -0400

    I don't know why this broke but wtv

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,8 +1,5 @@
 import rss from '@astrojs/rss';
 import { getCollection } from 'astro:content';
-import sanitizeHtml from 'sanitize-html';
-import MarkdownIt from 'markdown-it';
-const parser = new MarkdownIt();
 
 export async function get(context) {
     const work = await getCollection('work');
@@ -18,7 +15,6 @@ export async function get(context) {
                 // Compute RSS link from post `slug`
                 // This example assumes all posts are rendered as `/blog/[slug]` routes
                 link: `/work/${post.slug}/`,
-                content: sanitizeHtml(parser.render(post.body)),
         })),
     });
 }
