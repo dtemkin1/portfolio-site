615915273003d4348a67d69191494a703829f34f
Dgoat724236 <dtemkin@mit.edu>
2023-07-05 14:03:03 -0400

    Update sitemap (and dependencies)

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,15 @@ import cloudflare from "@astrojs/cloudflare";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://diegotemkin.com',
-  integrations: [sitemap(), compress()],
+  integrations: [sitemap({
+    customPages: [
+      'https://diegotemkin.com/work/mostec-2021',
+      'https://diegotemkin.com/work/mit-athena-site',
+      'https://diegotemkin.com/work/edd',
+      'https://diegotemkin.com/work/mini-urop-2023',
+      'https://diegotemkin.com/work/psfc'
+    ],
+  }), compress()],
   output: "server",
   adapter: cloudflare()
 });
