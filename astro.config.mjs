4c32a5fe534045d2ced1735f238a43957e40997c
Dgoat724236 <dtemkin@mit.edu>
2023-07-08 19:41:28 -0400

    Switch to netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,22 +1,16 @@
 import { defineConfig } from 'astro/config';
 import compress from "astro-compress";
 import sitemap from "@astrojs/sitemap";
-import cloudflare from "@astrojs/cloudflare";
+import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://diegotemkin.com',
   integrations: [sitemap({
-    customPages: [
-      'https://diegotemkin.com/work/mostec-2021/',
-      'https://diegotemkin.com/work/mit-athena-site/',
-      'https://diegotemkin.com/work/edd/',
-      'https://diegotemkin.com/work/mini-urop-2023/',
-      'https://diegotemkin.com/work/psfc/'
-    ],
-    filter: (page) => page !== 'https://diegotemkin.com/rss.xml/',
+    customPages: ['https://diegotemkin.com/work/mostec-2021/', 'https://diegotemkin.com/work/mit-athena-site/', 'https://diegotemkin.com/work/edd/', 'https://diegotemkin.com/work/mini-urop-2023/', 'https://diegotemkin.com/work/psfc/'],
+    filter: page => page !== 'https://diegotemkin.com/rss.xml/'
   }), compress()],
   output: "server",
-  adapter: cloudflare(),
+  adapter: netlify(),
   compressHTML: true
 });
