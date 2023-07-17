5abbc0095e6a9467bcf81e762278b32c319f28d6
Dgoat724236 <dtemkin@mit.edu>
2023-07-16 22:20:10 -0400

    Fix...

--- astro.config.mjs
+++ astro.config.mjs
@@ -13,7 +13,7 @@ const files = fs.readdirSync(directoryPath);
 const siteUrl = 'https://www.diegotemkin.com';
 const blogUrls = files.map((file) => {
   const fileName = file.split('.')[0];
-  return `${siteUrl}/${fileName}`;
+  return `${siteUrl}/work/${fileName}`;
 });
 
 // https://astro.build/config
