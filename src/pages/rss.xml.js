e8fffb5565ee64a1aaa028f74092887bce2a7284
Dgoat724236 <dtemkin@mit.edu>
2023-07-24 21:57:55 -0400

    Add image to RSS

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -6,19 +6,29 @@ const parser = new MarkdownIt();
 
 export async function get(context) {
     const work = await getCollection('work');
-        return rss({
-            title: 'Diego Temkin',
-            description: 'The personal site of Diego Temkin',
-            site: context.site,
-            customData: `<language>en-us</language>`,
-            items: work.map((post) => ({
-                title: post.data.title,
-                pubDate: post.data.publishDate,
-                description: post.data.description,
-                // Compute RSS link from post `slug`
-                // This example assumes all posts are rendered as `/blog/[slug]` routes
-                link: `/work/${post.slug}/`,
-                content: sanitizeHtml(parser.render(post.body)),
+    return rss({
+        title: 'Diego Temkin',
+        description: 'The personal site of Diego Temkin',
+        site: context.site,
+        xmlns: {
+            media: "http://search.yahoo.com/mrss/",
+        },
+        customData: `<language>en-us</language>`,
+        items: work.map((post) => ({
+            title: post.data.title,
+            pubDate: post.data.publishDate,
+            description: post.data.description,
+            // Compute RSS link from post `slug`
+            // This example assumes all posts are rendered as `/blog/[slug]` routes
+            link: `/work/${post.slug}/`,
+            content: sanitizeHtml(parser.render(post.body)),
+            customData: `<media:content
+                type="image/${post.data.img.format == "jpg" ? "jpeg" : "png"}"
+                width="${post.data.img.width}"
+                height="${post.data.img.height}"
+                medium="image"
+                url="${context.site + post.data.img.src}" />
+            `,
         })),
     });
 }
