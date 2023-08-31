1f3adc2fc3be04ca770969614f73b1d17b13688b
dtemkin1 <65834932+dtemkin1@users.noreply.github.com>
2023-08-31 18:52:28 -0400

    remove sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,30 +1,10 @@
 import { defineConfig } from 'astro/config';
-import sitemap from '@astrojs/sitemap';
 import netlify from '@astrojs/netlify/functions';
-
-// code for sitemap
-import url from 'node:url';
-import path from 'node:path';
-import fs from 'node:fs';
-const __filename = url.fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const directoryPath = path.join(__dirname, 'src', 'content', 'work');
-const files = fs.readdirSync(directoryPath);
-const siteUrl = 'https://www.diegotemkin.com';
-const blogUrls = files.map((file) => {
-	const fileName = file.split('.')[0];
-	return `${siteUrl}/work/${fileName}/`;
-});
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://www.diegotemkin.com',
-	integrations: [
-		sitemap({
-			customPages: blogUrls,
-			filter: (page) => !(page in ['https://www.diegotemkin.com/rss.xml/']),
-		}),
-	],
+	integrations: [],
 	output: 'server',
 	adapter: netlify(),
 });
