dcf70fdf8ae63913c359a51647148cdce1091f57
Diego Temkin <65834932+dtemkin1@users.noreply.github.com>
2024-06-24 21:28:41 -0400

    update to new eslint

---
+++ eslint.config.js
@@ -0,0 +1,12 @@
+// @ts-check
+
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import eslintPluginAstro from 'eslint-plugin-astro';
+
+export default tseslint.config(
+	eslint.configs.recommended,
+	...tseslint.configs.recommended,
+	...eslintPluginAstro.configs.recommended,
+	...eslintPluginAstro.configs['jsx-a11y-recommended']
+);
