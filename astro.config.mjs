0dbb940a7d3c75053d34fd9ebec7b5b387de53aa
Diego Temkin <65834932+dtemkin1@users.noreply.github.com>
2025-10-26 15:57:36 -0400

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,9 +5,5 @@ import sitemap from "@astrojs/sitemap";
 // https://astro.build/config
 export default defineConfig({
   site: "https://www.diegotemkin.com",
-  integrations: [sitemap()],
-  prefetch: true,
-  experimental: {
-    clientPrerender: true,
-  },
+  integrations: [sitemap()]
 });
